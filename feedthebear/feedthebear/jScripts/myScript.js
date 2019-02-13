5b8f6d79bebe84c1d9d20b9d3ae82925f9193727
haimraziev <37425530+haimraziev@users.noreply.github.com>
2019-02-13 03:09:05 +0200

    Add files via upload

--- feedthebear/feedthebear/jScripts/myScript.js
+++ feedthebear/feedthebear/jScripts/myScript.js
@@ -6,21 +6,14 @@
     $(".howToPlay").click(function () {
         $("#howToPlayDiv").toggle();
     });
-	
-	 $(".copyright").click(function () {
-        $("#copyrightDiv").toggle();
-    });
 
     $(".closeAbout").click(function () {
         $("#aboutDiv").hide();
+        $("#gameIframe")[0].contentWindow.focus();
     });
 
     $(".closeHowToPlay").click(function () {
         $("#howToPlayDiv").hide();
+        $("#gameIframe")[0].contentWindow.focus();
     });
-	
-	$(".closecopyright").click(function () {
-        $("#copyrightDiv").hide();
-    });
-	
 });
